187caaf4a7b0ae3f45d6c3bafc14e865c8b3f184
Krishan Patel <krishan711@gmail.com>
2020-11-06 13:01:07 +0000

    .

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -e -o pipefail
 
 name="everysize-app"
 url="${name}.kibalabs.com"
-dockerImageName="registry.gitlab.com/kibalabs/everypage/everysize-app"
+dockerImageName="docker.pkg.github.com/kibalabs/everysize-app"
 dockerTag="latest"
 dockerImage="${dockerImageName}:${dockerTag}"
 version="$(git rev-list --count HEAD)"
